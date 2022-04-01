6bf4bd83d9b0f98b121647ebbe742d386250fe8f
NatalieHumphriesGitHub <natalie.humphries@outlook.com>
2022-04-01 12:30:21 +0000

    Added team generator code

---
+++ team-generator/application/routes.py
@@ -0,0 +1,16 @@
+from application import app
+from flask import jsonify
+from random import choice
+
+teams = ['Man City', 'Liverpool', 'Chelsea', 'Arsenal', 'Tottenham', 'Man Utd', 'West Ham', 'Wolves', 'Aston Villa', 'Leicester City', 'Southampton', 'Crystal Palace', 'Brighton', 'Newcastle', 'Brentford', 'Leeds Utd', 'Everton', 'Watford', 'Burnley', 'Norwich']
+
+@app.route('/get-teams', methods=['GET'])
+def get_teams():
+    home_team = choice(teams)
+    away_team = choice(teams)
+    while away_team != home_team:
+        return jsonify(home_team=home_team, away_team=away_team)
+    else:
+        return "Teams can't play themselves!"
+        
+
