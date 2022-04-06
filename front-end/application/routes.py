ab77c4e87e55bdb0c741be2a66be2b982780ee82
NatalieHumphriesGitHub <natalie.humphries@outlook.com>
2022-04-06 09:55:04 +0000

    updated code for home-away teams being the same

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -8,7 +8,7 @@ from datetime import datetime
 def index():
     home_team = requests.get('http://home-team-generator:5000/get-home-team').json()   #this is a dictionary because we are just pulling out the json object from the get request
     away_team = requests.get('http://away-team-generator:5000/get-away-team').json()  #as above
-    while away_team == home_team:
+    while away_team["away_team"] == home_team["home_team"]:
         away_team = requests.get('http://away-team-generator:5000/get-away-team').json() #this is another get request until the home and away teams are not identical
     team_score = requests.post('http://score-generator:5000/team-scores', json=dict(home_team=home_team["home_team"], away_team=away_team["away_team"])) #we're posting 2 keys to the team score function, which will then return 2 values
     home_team_score = team_score.json()["home_team_score"] #this is pulling out the home team score from the json object produced in the team-scores route - the result of this this is now just a value and no longer a dictionary
